865b8c71ca8f1ff001b22b712ce3bed8d5e0ae3a
Ivan <inferal73@gmail.com>
2018-01-12 23:53:17 +0400

    добавляет верстку для топ бара

--- index.php
+++ index.php
@@ -1,11 +1,56 @@
 <?php
-$title = "Главная";
+$title = "Яндекс.Переговорки";
 $page = "index";
 include('include/header.php');
 ?>
 
 <main class="page-body">
+    <div class="workspace">
+        <div class="workspace-top-bar">
+            <div class="workspace-date-controls">
+                <div class="button-with-icon">
+                    <svg width="10" height="10">
+                        <use xlink:href="#arrow-left"></use>
+                    </svg>
+                </div>
+                <a href="#" class="workspace-calendar-title"><h3>14 дек · Сегодня</h3></a>
+                <div class="button-with-icon">
+                    <svg width="10" height="10">
+                        <use xlink:href="#arrow-right"></use>
+                    </svg>
+                </div>
+            </div>
+            <div class="workspace-time-grade">
+                <span class="passed">8:00</span>
+                <div class="workspace-current-time" style="left: 19.5%;">
+                    11:05
+                </div>
+                <span class="passed">9</span>
+                <span class="passed">10</span>
+                <span class="passed">11</span>
+                <span>12</span>
+                <span>13</span>
+                <span>14</span>
+                <span>15</span>
+                <span>16</span>
+                <span>17</span>
+                <span>18</span>
+                <span>19</span>
+                <span>20</span>
+                <span>21</span>
+                <span>22</span>
+                <span>23</span>
+            </div>
+        </div>
+        <div class="workspace-body">
+            <div class="workspace-left-bar">
 
+            </div>
+            <div class="workspace-time-line">
+
+            </div>
+        </div>
+    </div>
 </main>
 
 </body>
