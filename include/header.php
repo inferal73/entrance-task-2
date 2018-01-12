5e3178d9bf37d740cae596d212c2c662368a9c70
Ivan <inferal73@gmail.com>
2018-01-12 10:21:11 +0400

    добавляет фавикон

--- include/header.php
+++ include/header.php
@@ -5,13 +5,7 @@
                 <title><?=$title?></title>
                 <meta name="description" content="">
                 <meta name="viewport" content="width=device-width">
-                <script>
-                  document.onload = function () {
-                    document.body.className = document.body.className.replace('loading', '');
-                  };
-                  /* PubSub */
-                  window.PS=(function(){var topics={};var hOP=topics.hasOwnProperty;return{subscribe:function(topic,listener){if(!hOP.call(topics,topic))topics[topic]=[];var index=topics[topic].push(listener)-1;return{remove:function(){delete topics[topic][index];}};},publish:function(topic,info){if(!hOP.call(topics,topic))return;topics[topic].forEach(function(item){item(info!==undefined?info:{});});}};})();
-                </script>
+                <meta name="theme-color" content="#ffffff">
                 <style>
                     /* html5doctor.com Reset v1.6.1 - http://cssreset.com */
                     html,body,div,span,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,abbr,address,cite,code,del,dfn,em,img,ins,kbd,q,samp,small,strong,sub,sup,var,b,i,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,figcaption,figure,footer,header,hgroup,menu,nav,section,summary,time,mark,audio,video{margin:0;padding:0;border:0;outline:0;font-size:100%;vertical-align:baseline;background:transparent}body{line-height:1}article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section,main{display:block}nav ul{list-style:none}blockquote,q{quotes:none}blockquote:before,blockquote:after,q:before,q:after{content:none}a{margin:0;padding:0;font-size:100%;vertical-align:baseline;background:transparent}ins{background-color:#ff9;color:#000;text-decoration:none}mark{background-color:#ff9;color:#000;font-style:italic;font-weight:bold}del{text-decoration:line-through}abbr[title],dfn[title]{border-bottom:1px dotted;cursor:help}table{border-collapse:collapse;border-spacing:0}hr{display:block;height:1px;border:0;border-top:1px solid #ccc;margin:1em 0;padding:0}input,select{vertical-align:middle}
@@ -43,6 +37,11 @@
 
                     }
                 </style>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+                <link rel="manifest" href="/manifest.json">
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
                 <link rel="stylesheet" href="/dist/css/style.css">
             </head>
 <body data-page="<?=$page?>" class="<?=$page?>">
